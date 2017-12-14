e3a882d4eceead7376fc74c88d259b8eeb6f4bd1
Richie Lin <bilbo184@gmail.com>
2017-12-13 22:23:38 -0800

    Update 6kyu_ArrayDeepCount.js

--- 6kyu_ArrayDeepCount.js
+++ 6kyu_ArrayDeepCount.js
@@ -24,3 +24,8 @@ function deepCount(arr) {
     }
     return temp;
 }
+
+
+console.log(deepCount([1, 2, 3])); // 3
+console.log(deepCount(["x", "y", ["z"]])); // 4
+console.log(deepCount([1, 2, [3, 4, [5]]])); // 7 
