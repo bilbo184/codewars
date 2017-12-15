a0d0b244f638a8905d74def5775d3c205f540725
Richie Lin <bilbo184@gmail.com>
2017-12-14 23:32:12 -0800

    Update 6kyu_CaseReversalOfConsecutiveDuplicates.js

--- 6kyu_CaseReversalOfConsecutiveDuplicates.js
+++ 6kyu_CaseReversalOfConsecutiveDuplicates.js
@@ -1,3 +1,28 @@
-let reverseCase = function(word){
-    return word.replace(RegExp(/(.)\1{1,}/, "g"), a => /[A-Z]/.test(a) ? a.toLowerCase() : a.toUpperCase());
+// The aim of this Kata is to write a function which will reverse the case of all consecutive duplicate letters in a string. That is, any letters that occur one after the other and are identical.
+
+// If the duplicate letters are lowercase then they must be set to uppercase, and if they are uppercase then they need to be changed to lowercase.
+
+// Examples:
+
+// reverseCase("puzzles")    Expected Result: "puZZles"
+// reverseCase("massive")    Expected Result: "maSSive"
+// reverseCase("LITTLE")     Expected Result: "LIttLE"
+// reverseCase("shhh")       Expected Result: "sHHH"
+
+
+function reverseCase(str){
+return str.replace(RegExp(/(.)\1{1,}/, "g"), a => /[A-Z]/.test(a) ? a.toLowerCase() : a.toUpperCase());
 }
+
+// function reverseCase(string) {
+//   return string.replace(/([a-zA-Z])\1+/g, v => v.toLowerCase() === v ? v.toUpperCase() : v.toLowerCase());
+// }
+
+// function reverseCase(string) {
+//   return string.replace(/(.)\1+/g, x => x == x.toUpperCase() ? x.toLowerCase() : x.toUpperCase());
+// }
+
+console.log(reverseCase("puzzles"));
+console.log(reverseCase("massive"));
+console.log(reverseCase("LITTLE"));
+console.log(reverseCase("shhh"));
