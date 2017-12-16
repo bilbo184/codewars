3af712445e4f7303b02620fc4047f23f3a919c25
Richie Lin <bilbo184@gmail.com>
2017-12-15 18:10:26 -0800

    Update 6kyu_DashatizeIt.js

--- 6kyu_DashatizeIt.js
+++ 6kyu_DashatizeIt.js
@@ -3,9 +3,23 @@
 // Given a number, return a string with dash'-'marks before and after each odd integer, 
 // but do not begin or end the string with a dash mark.
 
-// Ex:
 
-// dashatize(274) -> '2-7-4'
-// dashatize(6815) -> '68-1-5'
+function dashatize(num) {
+  var str = Math.abs(num).toString().split(/([13579])/g).filter(function(a){
+    return a !== '';
+  }).join("-");
+  
+  console.log(str);
+}
 
-let dashatize = num => Math.abs(num).toString().split(/([13579])/g).filter(a => a !== '').join('-');
+console.log(dashatize(274)); //'2-7-4'
+console.log(dashatize(6815)); // '68-1-5'
+
+// solution 2
+
+// function dashatize(num) {
+//   return String(num)
+//     .replace(/([13579])/g, "-$1-")
+//     .replace(/--+/g, "-")
+//     .replace(/(^-|-$)/g, "")
+// }
