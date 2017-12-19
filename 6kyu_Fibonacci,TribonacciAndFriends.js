033cd3d35bd46c67ea56a45b1815e0ac3c01836b
Richie Lin <bilbo184@gmail.com>
2017-12-18 19:25:18 -0800

    Update 6kyu_Fibonacci,TribonacciAndFriends.js

--- 6kyu_Fibonacci,TribonacciAndFriends.js
+++ 6kyu_Fibonacci,TribonacciAndFriends.js
@@ -36,3 +36,12 @@ console.log(Xbonacci([1,1],10)); // [ 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ]
 console.log(Xbonacci([0,0,0,0,1],12)); // [ 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 31, 61 ]
 console.log(Xbonacci([1,0,0,0,0,0,1],10)); // [ 1, 0, 0, 0, 0, 0, 1, 2, 3, 6 ]
 console.log(Xbonacci([1,0,0,0,0,0,0,0,0,0],20)); // [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 4, 8, 16, 32, 64, 128, 256 ]
+
+// solution 2
+
+const Xbonacci = (sig, n) => {
+  let len = sig.length;
+  for (let i = len; i < n; i++) 
+    sig[i] = sig.slice(i - len).reduce((a, b) => a + b);
+  return sig.slice(0, n);
+}
