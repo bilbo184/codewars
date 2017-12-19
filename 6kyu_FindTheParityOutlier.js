eb952e295cb9103e7d5199c5219a3aae21e605f7
Richie Lin <bilbo184@gmail.com>
2017-12-19 12:31:22 -0800

    Update 6kyu_FindTheParityOutlier.js

--- 6kyu_FindTheParityOutlier.js
+++ 6kyu_FindTheParityOutlier.js
@@ -17,8 +17,32 @@
 
 // Should return: 160
 
-let even = num => num % 2 === 0;
+function findOutlier(arr){
+  var arr1 = [];
+  var arr2 = [];
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i]%2 === 1 || arr[i]%2 === -1){
+      arr1.push(arr[i]);
+    }else{
+      arr2.push(arr[i]);
+    }
+  }
+  return arr1.length === 1 ? arr1[0] : arr2[0];
+}
 
-let odd = num => Math.abs(num % 2) == 1;
+console.log(findOutlier([0, 1, 2])); // 1
+console.log(findOutlier([1, 2, 3])); // 2
+console.log(findOutlier([2,6,8,10,3])); // 3
+console.log(findOutlier([0,0,3,0,0])); // 3
+console.log(findOutlier([1,1,0,1,1])); // 0
+console.log(findOutlier([-126419384,-77748715,-137272164,-184622374,-131364808,172316244,171161972,112109140,37039864,-60033196,-110003260,-124425118,-67564184,82344226,-74237446,-196304104,-160035058,-92784302]));
+// -77748715
 
-let findOutlier = ints => ints.slice(0,3).filter(even).length > 1 ? ints.find(odd) : ints.find(even);
+
+// solution 2
+
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
