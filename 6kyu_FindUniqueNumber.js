8e49f73f9456062bfe57b34b8b870753db19056d
Richie Lin <bilbo184@gmail.com>
2017-12-19 17:08:26 -0800

    Update 6kyu_FindUniqueNumber.js

--- 6kyu_FindUniqueNumber.js
+++ 6kyu_FindUniqueNumber.js
@@ -14,10 +14,10 @@ function findUniq(arr) {
   arr = arr.sort(function(a, b){
     return a - b;
   });
-  console.log(arr);
- return arr[0] == arr[1] ? arr.pop():arr[0];
+ return arr[0] == arr[1] ? arr[arr.length-1] : arr[0];
 }
 
 console.log(findUniq([ 1, 1, 1, 1, 1, 1 ])); // 2 
 console.log(findUniq([ 0, 0, 0.55, 0, 0 ])); // 0.55
+console.log(findUniq([ 3, 10, 3, 3, 3 ])); // 10
 
