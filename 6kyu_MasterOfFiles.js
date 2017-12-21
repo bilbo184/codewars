0a77d76d2786bed736ac5e81e5bfc9b9a20f7848
Richie Lin <bilbo184@gmail.com>
2017-12-21 12:04:20 -0800

    Update 6kyu_MasterOfFiles.js

--- 6kyu_MasterOfFiles.js
+++ 6kyu_MasterOfFiles.js
@@ -20,8 +20,32 @@
 
 String.prototype.isAudio = function() {
   return /^[a-zA-Z]+\.(mp3|flac|alac|aac)$/.test(this);
-}
+};
 
 String.prototype.isImage = function() {
   return /^[a-zA-Z]+\.(jpg|jpeg|png|bmp|gif)$/.test(this);
-}
+};
+
+console.log("Nothing Else Matters.mp3".isAudio()); // false
+console.log("NothingElseMatters.mp3".isAudio()); // true
+console.log("DaftPunk.FLAC".isAudio()); // false
+console.log("DaftPunk.flac".isAudio()); // true
+console.log("AmonTobin.aac".isAudio()); // true
+console.log(" Amon Tobin.alac".isAudio()); // false
+console.log("tobin.alac".isAudio()); // true
+console.log("Home.jpg".isImage()); // true
+console.log("flat.jpeg".isImage()); // true
+console.log("icon2.jpg".isImage()); //  false
+console.log("bounce.gif".isImage()); // true
+console.log("animate bounce.GIF".isImage()); // false
+console.log("transparency.png".isImage()); // true
+
+// solution 2
+
+// String.prototype.isAudio = function() {
+//   return /^[a-zA-Z]+\.(mp3|flac|al?ac)$/.test(this)
+// }
+
+// String.prototype.isImage = function() {
+//   return /^[a-zA-Z]+\.(jpe?g|png|bmp|gif)$/.test(this)
+// }
