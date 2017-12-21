7e153ceec935d78a0590962e889b09530ecf3dbe
Richie Lin <bilbo184@gmail.com>
2017-12-20 16:32:42 -0800

    Update 6kyu_IfYouCanReadThis.js

--- 6kyu_IfYouCanReadThis.js
+++ 6kyu_IfYouCanReadThis.js
@@ -12,16 +12,21 @@
 // Keep the punctuation, and remove the spaces.
 // Use Xray without dash or space.
 
-let to_nato = (function() {
-    let nato = {
+
+function to_nato(words){
+  var nato = {
       'a' : "Alfa",'b' : "Bravo","c" : "Charlie","d" : "Delta","e" : "Echo",
       "f" : "Foxtrot","g" : "Golf","h" : "Hotel","i" : "India","j" : "Juliett",
       "k" : "Kilo","l" : "Lima","m" : "Mike","n" : "November","o" : "Oscar",
       "p" : "Papa","q" : "Quebec","r" : "Romeo","s" : "Sierra","t" : "Tango",
       "u" : "Uniform","v" : "Victor","w" : "Whiskey","x" : "Xray","y" : "Yankee","z" : "Zulu"
-  }
-  
-  return function(words) {
-      return words.toLowerCase().replace(/\s*/g, "").split("").map(a => (/[a-z]/i.test(a)) ? nato[a] + " " : a + " ").join("").trim();
-  }
-})();
+  };
+  return words.toLowerCase().replace(/\s*/gi, "").split('').map(function(a){
+    return (/[a-z]/i.test(a)) ? nato[a] + " " : a + " ";
+  }).join('').trim();
+}
+ 
+console.log(to_nato('If you can read')); // India Foxtrot Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta
+
+console.log(to_nato('Did not see that coming')); // Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf
+
