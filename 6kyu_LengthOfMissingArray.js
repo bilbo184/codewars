d76120590602fa56be646cc23fa5e666bb51dda9
Richie Lin <bilbo184@gmail.com>
2017-12-20 23:53:38 -0800

    Update 6kyu_LengthOfMissingArray.js

--- 6kyu_LengthOfMissingArray.js
+++ 6kyu_LengthOfMissingArray.js
@@ -24,14 +24,27 @@
 
 // I have created other katas. Have a look if you like coding and challenges.
 
+
 function getLengthOfMissingArray(arrayOfArrays) {
-    arrayOfArrays = (arrayOfArrays) ? arrayOfArrays.map(a=> a ? a.length : 0) : [0];
-    let maxSize =  Math.max(...arrayOfArrays), minSize = Math.min(...arrayOfArrays);
-    if(minSize === 0) { return 0; }
-    for(let i = minSize; i < maxSize; i++) {
-        if(arrayOfArrays.indexOf(i) === -1) {
-            return i;
-        }
-    }
+  var lengths = (arrayOfArrays || [])
+    .map(a => a ? a.length : 0)
+    .sort((a, b) => a - b);
+  if (lengths.includes(0)) {
     return 0;
+  }
+  for (var i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1;
+    }
+  }
+  return 0;
 }
+
+
+console.log(getLengthOfMissingArray([ [ 1, 2 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]] )); // 3
+console.log(getLengthOfMissingArray([ [ 5, 2, 9 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]])); // 2
+console.log(getLengthOfMissingArray([ [ null ], [ null, null, null ] ])); // 2
+console.log(getLengthOfMissingArray([ [ 'a', 'a', 'a' ], [ 'a', 'a' ], [ 'a', 'a', 'a', 'a' ], [ 'a' ], [ 'a', 'a', 'a', 'a', 'a', 'a' ]])); // 5
+console.log(getLengthOfMissingArray([])); // 0
+
+
