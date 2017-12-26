cdabc61a2d147141a4811475af07db0334e23aaa
Richie Lin <bilbo184@gmail.com>
2017-12-25 23:09:21 -0800

    Update 6kyu_RemoveAnchorFromUrl.js

--- 6kyu_RemoveAnchorFromUrl.js
+++ 6kyu_RemoveAnchorFromUrl.js
@@ -1,3 +1,15 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+
+
 function removeUrlAnchor(url){
   return url.split("#").shift();
 }
+
+
+
+console.log(removeUrlAnchor('www.codewars.com#about'));
+// returns 'www.codewars.com'
+
+console.log(removeUrlAnchor('www.codewars.com?page=1'));
+// returns 'www.codewars.com?page=1' 
