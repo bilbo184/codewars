9a6a0fc496464fc46cfc0abe3c5d67f88e907a08
Richie Lin <bilbo184@gmail.com>
2017-12-26 23:48:05 -0800

    Update 6kyu_ReverseVowelsInAString.js

--- 6kyu_ReverseVowelsInAString.js
+++ 6kyu_ReverseVowelsInAString.js
@@ -12,11 +12,26 @@
 
 function reverseVowels(str) {
   str = str.split("");
-  let vowels = str.filter(a => a.match(/[aeiou]/gi));
-  for(let i = str.length-1; i >= 0; i--){
+  var vowels = str.filter(function(a){
+    return a.match(/[aeiou]/gi);
+  });
+  for(var i = str.length-1; i >= 0; i--){
     if(str[i].match(/[aeiou]/gi)){
       str[i] = vowels.shift();
     }
   }
-  return str.join("");
+  return str.join('');
+}
+
+
+
+console.log(reverseVowels("Hello!")); // "Holle!"
+console.log(reverseVowels("Tomatoes")); // "Temotaos"
+console.log(reverseVowels("Reverse Vowels In A String")); // "RivArsI Vewols en e Streng"
+
+// solution 2
+
+function reverseVowels(str) {
+  const vowels = str.match(/[aeiou]/gi);
+  return (vowels === null) ? str : str.replace(/[aeiou]/gi, c => vowels.pop());
 }
