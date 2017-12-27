048a87bb98860449f82b5d18132dfd2bc0e6cf13
Richie Lin <bilbo184@gmail.com>
2017-12-26 18:12:02 -0800

    Update 6kyu_ReverseWords.js

--- 6kyu_ReverseWords.js
+++ 6kyu_ReverseWords.js
@@ -7,6 +7,15 @@
 
 // reverse_words("This is an example!") # returns  "sihT si na !elpmaxe"
 
-let reverseWords = function(str) {
-  return str.split(" ").map( a => a.split("").reverse().join("")).join(" ");
+function reverseWords(str){
+  return str.split(' ').map(function(val, i){
+    return val.split('').reverse().join('');
+  }).join(' ');
 }
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.')); // 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+console.log(reverseWords('apple')); // 'elppa'
+console.log(reverseWords('a b c d')); // 'a b c d'
+console.log(reverseWords('double  spaced  words')); // 'elbuod  decaps  sdrow'
+
