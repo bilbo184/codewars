62deb97631a9907550022611daab0312ed34a792
Richie Lin <bilbo184@gmail.com>
2017-12-28 14:41:41 -0800

    Update 6kyu_SimpleFun#217_SortByGuide.js

--- 6kyu_SimpleFun#217_SortByGuide.js
+++ 6kyu_SimpleFun#217_SortByGuide.js
@@ -44,9 +44,16 @@
 // [78, 4, 3, 45, 56, 66, 2, 2, 4].
 
 function sortByGuide(arr, guide) {
-    let temp = [], 
-        num = guide.filter(a => a !== -1).sort((a,b) => a - b);
-    for(let i = 0; i < num.length; i++)
-        temp.push(arr[guide.indexOf(num[i])])
-    return arr.map((a, i) => (guide[i] !== -1) ? temp.shift() : a);
+   var temp = [];
+       num = guide.filter(a => a !== -1).sort((a, b) => a - b);
+       for(var i = 0; i < num.length; i++){
+         temp.push(arr[guide.indexOf(num[i])]);
+       }
+       return arr.map((a, i) => (guide[i] !== -1) ? temp.shift() : a);
 }
+console.log(sortByGuide([56, 78, 3, 45, 4, 66, 2, 2, 4],[3, 1, -1, -1, 2, -1, 4, -1, 5])); // 
+console.log(sortByGuide([45, 56, 78],[-1, 2, 1])); // 
+console.log(sortByGuide([2, 5, 3, 1, 4, 70, 8],[2, 5, 1, 3, -1, 4, -1])); // 
+console.log(sortByGuide([700, 800, 400, 100, 900, 325],[2, -1, 1, -1, 3, -1])); // 
+console.log(sortByGuide([70, 10, 15, 800, 400, 4, 225, 438, 509, 1000],[6, 1, 4, -1, -1, 2, -1, -1, 5, 3])); // 
+console.log(sortByGuide([27, 67, 80, 38, 21], [2, 5, 3, 1, 4])); // 
