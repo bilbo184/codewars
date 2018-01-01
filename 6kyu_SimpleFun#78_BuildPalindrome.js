e9aef9d8b13ba09ac3dc567309c5671ab9bf4eb7
Richie Lin <bilbo184@gmail.com>
2018-01-01 14:59:58 -0800

    Update 6kyu_SimpleFun#78_BuildPalindrome.js

--- 6kyu_SimpleFun#78_BuildPalindrome.js
+++ 6kyu_SimpleFun#78_BuildPalindrome.js
@@ -16,16 +16,22 @@
 // Constraints: 3 ≤ str.length ≤ 10.
 // [output] a string
 
-function buildPalindrome(str) {
-  let temp = str;
-  
-	function isPalindrome(str) {
-	    return str.split("").reverse().join("") === str
-	}
-  
-  for(let i =0; i < str.length; i++){
-      temp = str + str.slice(0, i).split("").reverse().join("");
-      if(isPalindrome(temp)){ break; }
-  }
-  return temp;
+
+function isPalindrome(str){
+   return str.split('').reverse().join('') === str;
 }
+
+function buildPalindrome(str) {
+  var temp = str;
+  for(var i = 0; i < str.length; i++){
+    temp = str + str.slice(0, i).split('').reverse().join('');
+    if(isPalindrome(temp)){
+     return temp;
+    }
+  }
+
+}
+
+console.log(buildPalindrome("abcdc")); // abcdcba
+console.log(buildPalindrome("ababab")); // abababa
+
