cd3c1ae28146b68c2a52e035cc49e08efd954e71
Richie Lin <bilbo184@gmail.com>
2018-01-02 14:38:05 -0800

    Update 6kyu_SimpleFun#79_DeleteADigit.js

--- 6kyu_SimpleFun#79_DeleteADigit.js
+++ 6kyu_SimpleFun#79_DeleteADigit.js
@@ -15,19 +15,29 @@
 // Constraints: 10 ≤ n ≤ 1000000.
 // [output] an integer
 
+
 function deleteDigit(n) {
-    n = String(n);
-    for(let i = 0; i < n.length-1; i++){
-        if(n[i] < n[i+1] && n[i] !== 0){
-            return +(n.slice(0, i)+ n.slice(i+1))
-        }
+  var num = [];
+  n = String(n);
+  var Max = 0;
+  for (var i = 0; i < n.length; i++) {
+    if(Max < n.replace(n[i], '')){
+      Max = n.replace(n[i], '');
     }
-    for(let i = 0; i < 9; i++){
-        let idx =  n.indexOf(String(i))
-        if(idx > -1){
-            n = n.slice(0, idx) + n.slice(idx+1);
-            break;
-        }    
-    }
-    return +n;
+  }
+  return Number(Max);
+
 }
+// function deleteDigit(n) {
+//   var num = [];
+//   n = (n+'');
+  
+//   for (var i = 0; i < n.length; i++) {
+//     num.push(n.replace(n[i], ''));
+//   }
+//   return Math.max(...num);
+// }
+console.log(deleteDigit(152)); // 52
+console.log(deleteDigit(1001)); // 101
+console.log(deleteDigit(10)); // 1
+console.log(deleteDigit(189)); // 89
