edc0f93c0c25a0516748632e67880c65d2a8f906
Richie Lin <bilbo184@gmail.com>
2018-01-02 23:25:23 -0800

    Update 6kyu_SortTheOdd.js

--- 6kyu_SortTheOdd.js
+++ 6kyu_SortTheOdd.js
@@ -11,11 +11,12 @@
 
 // sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
 
-let sortArray = function(arr) {
-  let idx = arr.map((a, i) => a%2===1 ? i : '').filter(a => a !== '');
-  let odds = arr.filter(a => a % 2 === 1).sort((a,b) => a - b);
-  for(let i = 0; i < idx.length; i+=1) {
-    arr[idx[i]] = odds[i];
-  }
-  return arr;
+function sortArray(array) {
+  var odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+
+  return array.map((x) => x % 2 ? odd.shift() : x);
 }
+
+
+console.log(sortArray([5, 3, 2, 8, 1, 4])); // [ 1, 3, 2, 8, 5, 4 ]
+console.log(sortArray([5, 3, 1, 8, 0])); // [ 1, 3, 5, 8, 0 ]
