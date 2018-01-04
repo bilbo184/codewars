f7bf76e16041168ca283d49d27145ed70a2d54ec
Richie Lin <bilbo184@gmail.com>
2018-01-04 15:25:18 -0800

    Update 6kyu_TheOfficeV-FindAChair.js

--- 6kyu_TheOfficeV-FindAChair.js
+++ 6kyu_TheOfficeV-FindAChair.js
@@ -27,19 +27,31 @@
 // available, return 'Not enough!'
 
 
+
 function meeting(x, need){
-    let arr = [];
-    if(need === 0) { return "Game On"; }
-    for(let i = 0; i < x.length; i++){
-        let temp = 0;
-        if(x[i][1] > x[i][0].length)
-            temp = x[i][1] - x[i][0].length;
-        if(need - temp < 0)
-            temp = need;
-        need -= temp;
-        arr.push(temp);
-        if(need === 0) { return arr; }
+  var arr = [];
+  if(need === 0){
+    return "Game On";
+  }
+  for(var i in x){
+    var temp = 0;
+    if(x[i][1] > x[i][0].length){
+      temp = x[i][1] - x[i][0].length;
     }
-    
-    return (need > 0) ? "Not enough!" : arr;
+    if(need - temp <  0){
+      temp = need;
+    }
+      need = need - temp;
+      arr.push(temp);
+    if(need === 0){
+      return arr;
+    }
+
+  }
+  return (need > 0) ? "Not enough!" : arr;
 }
+
+console.log(meeting([['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9]], 4)); // [ 0, 1, 3 ]
+console.log(meeting([['XXX', 1], ['XXXXXX', 6], ['X', 2], ['XXXXXX', 8], ['X', 3], ['XXX', 1]], 5));
+// [ 0, 0, 1, 2, 2 ]
+console.log(meeting([['XX', 2], ['XXXX', 6], ['XXXXX', 4]], 0)); // Game On
