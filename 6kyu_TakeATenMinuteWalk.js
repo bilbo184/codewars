363835d6deb19c066c004ccd68c7509cd7c1d0aa
Richie Lin <bilbo184@gmail.com>
2018-01-04 14:17:36 -0800

    Update 6kyu_TakeATenMinuteWalk.js

--- 6kyu_TakeATenMinuteWalk.js
+++ 6kyu_TakeATenMinuteWalk.js
@@ -15,23 +15,19 @@
 // array (that's not a walk, that's standing still!).
 
 function isValidWalk(walk) {
-  let x = 0; y = 0;
-  if(walk.length != 10) { return false; }
-  for(let i = 0; i < walk.length; i += 1) {
-    switch(walk[i]) {
-      case ('n'): 
-        y+=1;
-        break;
-      case ('s'):
-        y-=1
-        break;
-      case ('w'):
-        x += 1;
-        break;
-      default:
-        x -= 1;
-        break;
-    }
+  var directions = {
+    n : 0,
+    s : 0,
+    w : 0,
+    e : 0
   }
-  return (x === 0 && y === 0);
+  walk.forEach(function(a){
+    directions[a]++;
+  });
+  return (walk.length === 10) && (directions.n === directions.s) &&(directions.w === directions.e);
 }
+
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])); // true
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])); // false
+console.log(isValidWalk(['w'])); // false
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])); // false
