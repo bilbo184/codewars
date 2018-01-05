5035060c1c264b6483a1ea8ec6e976ce377a540e
Richie Lin <bilbo184@gmail.com>
2018-01-05 15:05:12 -0800

    Update 6kyu_ValidPhoneNumber.js

--- 6kyu_ValidPhoneNumber.js
+++ 6kyu_ValidPhoneNumber.js
@@ -17,5 +17,9 @@
 // validPhoneNumber("(098) 123 4567")  => returns false
 
 function validPhoneNumber(phoneNumber){
-  return /^(\([0-9]{3}\)\s[0-9]{3}\-[0-9]{4})$/.test(phoneNumber);
+     return /^\((\d){3}\)\s(\d){3}\-(\d){4}$/.test(phoneNumber);
 }
+
+console.log(validPhoneNumber("(123) 456-7890")); // true
+console.log(validPhoneNumber("(1111)555 2345")); // false
+console.log(validPhoneNumber("(098) 123 4567")); // false
