33266a25732accffb38f3442246b2d011047cbc1
Richie Lin <bilbo184@gmail.com>
2018-01-06 01:08:41 -0800

    Update 6kyu_WhoLikesIt.js

--- 6kyu_WhoLikesIt.js
+++ 6kyu_WhoLikesIt.js
@@ -15,11 +15,29 @@
 // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 
 function likes(names) {
-  switch(names.length) {
-    case 0: return 'no one likes this';
-    case 1: return `${names[0]} likes this`;
-    case 2: return `${names[0]} and ${names[1]} like this`;
-    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-    default: return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;
+  switch(names.length){
+    case 0 : return "no one likes this";
+    case 1 : return  names[0] + " likes this";
+    case 2 : return  names[0] + " and " + names[1] + " like this";
+    case 3 : return  names[0] + ", " + names[1] + " and " + names[2] + " like this"; 
+    default : return  names[0] + ", " + names[1] + " and " + (names.length-2)  +" others like this"; 
   }
 }
+
+
+console.log(likes([])); // "no one likes this"
+console.log(likes(["Peter"])); // "Peter likes this"
+console.log(likes(["Jacob", "Alex"])); // "Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"])); // "Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // "Alex, Jacob and 2 others like this"
+
+
+// solution 2
+
+// function likes(names) {
+//   if(names.length === 0) return "no one likes this";
+//   if(names.length === 1) return names[0] + " likes this";
+//   if(names.length === 2) return names[0] + " and " + names[1] + " like this";
+//   if(names.length === 3) return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+//   return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+// }
