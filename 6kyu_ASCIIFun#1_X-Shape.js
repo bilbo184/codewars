dd67e040a87673a9f482b1b489f4a7b1ac057777
Richie Lin <bilbo184@gmail.com>
2018-01-09 15:04:55 -0800

    Update 6kyu_ASCIIFun#1_X-Shape.js

--- 6kyu_ASCIIFun#1_X-Shape.js
+++ 6kyu_ASCIIFun#1_X-Shape.js
@@ -19,10 +19,27 @@
 //                                      ■□□□■
 
 
-function x(n) {
-    let arr = Array(n).fill().map(_ => "□".repeat(n).split(""))
-    for(let i = 0; i < arr.length; i++) {
-        [arr[i][i], arr[i][arr.length-1-i]] = ["■","■"];
+// function x(n) {
+//   var arr = Array(n).fill().map(function(_){
+//     return "□".repeat(n).split("")
+//   })
+//   for(var i = 0; i < arr.length; i++){
+//   [arr[i][i], arr[i][arr.length-1-i]] = ["■","■"];
+//   }
+//   return arr.map(a=>a.join("")).join("\n");
+// }
+
+function x(n){
+  var output = "";
+  for(var i = 0; i < n; i++){
+    for(var j = 0; j < n; j++){
+      output = output + ((i===j || j === n-i-1) ? '■':'□') ;
     }
-    return arr.map(a=>a.join("")).join("\n");
+      output = output + ((i !== n-1) ? ('\n') : "");
+  }
+  return output;
 }
+
+console.log(x(5));
+console.log(x(7));
+console.log(x(11));
